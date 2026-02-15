a93e8135bf439808e2b677d575545e0f6b556522
Issika Kouamé <issikathereal@gmail.com>
2026-02-15 21:42:53 +0100

    Fix mongoose connection options

--- api/resume.js
+++ api/resume.js
@@ -9,10 +9,7 @@ async function connectToDatabase() {
     return cachedDb;
   }
 
-  const connection = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const connection = await mongoose.connect(process.env.MONGO_URI);
 
   cachedDb = connection;
   return connection;
